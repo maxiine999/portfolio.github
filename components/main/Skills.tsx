230a56272b1962175ae0cda2079dc7391d402677
maxiine999 <139379941+maxiine999@users.noreply.github.com>
2024-07-17 19:52:17 +0530

    Update Skills.tsx

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -52,15 +52,7 @@ const Skills = () => {
   
         <div className="w-full h-full absolute">
           <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
-            <video
-              className="w-full h-auto"
-              preload="false"
-              playsInline
-              loop
-              muted
-              autoPlay
-              src=""
-            />
+            
           </div>
         </div>
       
