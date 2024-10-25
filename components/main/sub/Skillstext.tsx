0845ed49f9a47685b92dfaf5535abbcfeb407bc9
Madhav Mishra <139379941+maxiine999@users.noreply.github.com>
2024-10-25 23:57:41 +0530

    Update Skillstext.tsx

--- components/main/sub/Skillstext.tsx
+++ components/main/sub/Skillstext.tsx
@@ -10,19 +10,14 @@ import { SparklesIcon } from "@heroicons/react/16/solid";
 
 const Skillstext = () => {
   return (
-    <div className="gap-4 translate-y-12 mt-7  w-full h-auto flex flex-col items-center justify-center">
+    <div className="gap-4 translate-y-12 mt-7 mb-10 w-full h-auto flex flex-col items-center justify-center">
       <motion.div
         variants={slideInFromLeft(0.5)}
         className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
       >
-        We Design, We Develop and Support Websites.
+        Designing, Developing and Supporting Websites.
       </motion.div>
-      <motion.div
-        variants={slideInFromRight(0.5)}
-        className=" text-[20px] text-gray-200 mb-10 mt-[0px] text-center"
-      >
-        Never miss a task, deadline or idea
-      </motion.div>
+     
     </div>
   );
 };
